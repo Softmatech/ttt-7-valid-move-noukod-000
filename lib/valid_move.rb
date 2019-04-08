d2d561dae192a665d3f4fa6370525b2055dab099
Andy Feidje Joseph <ingandy75@gmail.com>
2019-04-08 15:14:33 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -12,4 +12,10 @@ end
 
 def position_taken?(board,index)
     
+if board[index] == " " || board[index] == "" || board[index] == nil
+  return false
+else if board[index] == "X" || board[index] == "O"
+  return true
+end
+end
 end
